3a570834419a0e9d1db7efe84b58bceb1c8037d2
turiyviktor007-bit <turiyviktor007@gmail.com>
2026-06-28 13:48:34 +0300

    Update prom_filter_app.py

--- prom_filter_app.py
+++ prom_filter_app.py
@@ -340,7 +340,6 @@ class App(tk.Tk):
         if show:
             kw["show"] = show
         e = tk.Entry(parent, **kw)
-        e.configure(pady=8)
         return e
 
     def _btn_small(self, parent, text, cmd):
